0929fd98de9eb24847dec39cb7b156f17f0e1868
Kchau <Kchau061906@gmail.com>
2025-09-15 22:38:26 -0500

    Added linear and table layouts. Added initial values as hints. UI color change.

--- app/src/main/java/com/example/pokemon_tracker_from/MainActivity.java
+++ app/src/main/java/com/example/pokemon_tracker_from/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.pokemon_tracker_from;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +14,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_main_table);
+
+        EditText NatNumET = findViewById(R.id.NationalNumET);
+        EditText nameET = findViewById(R.id.nameET);
+        EditText speciesET = findViewById(R.id.speciesET);
+        EditText heightET = findViewById(R.id.heightET);
+        EditText weightET = findViewById(R.id.weightET);
+
     }
 }
