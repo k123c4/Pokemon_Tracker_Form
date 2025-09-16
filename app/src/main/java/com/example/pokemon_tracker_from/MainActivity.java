6be17decc1f9cac6e2be41aa02d6bb291235fa48
Kchau <Kchau061906@gmail.com>
2025-09-15 23:44:47 -0500

    Added word capitalization to Name and species

--- app/src/main/java/com/example/pokemon_tracker_from/MainActivity.java
+++ app/src/main/java/com/example/pokemon_tracker_from/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.pokemon_tracker_from;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
@@ -22,13 +23,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_linear);
 
-        EditText NatNumET = findViewById(R.id.NationalNumET);
+        EditText natNumET = findViewById(R.id.NationalNumET);
         EditText nameET = findViewById(R.id.nameET);
         EditText speciesET = findViewById(R.id.speciesET);
         RadioGroup genderGroup = findViewById(R.id.genderGroup);
         EditText heightET = findViewById(R.id.heightET);
         EditText weightET = findViewById(R.id.weightET);
-        Spinner lvlSp= findViewById(R.id.spinner);
+        Spinner lvlSp = findViewById(R.id.spinner);
+        EditText hpET = findViewById(R.id.HpET);
+        EditText attackET = findViewById(R.id.AttackET);
+        EditText defenceET = findViewById(R.id.defenceET);
+        Button resBut = findViewById(R.id.ResetBt);
+        Button subBut = findViewById(R.id.SaveBt);
+
 
         // list of numbers from 1 to 50
         List<String> numbers = new ArrayList<>();
@@ -43,5 +50,19 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         lvlSp.setAdapter(adapter);
 
+        //reset button function
+        resBut.setOnClickListener(v -> {
+            natNumET.setText("");
+            nameET.setText("");
+            speciesET.setText("");
+            heightET.setText("");
+            weightET.setText("");
+            hpET.setText("");
+            attackET.setText("");
+            defenceET.setText("");
+
+            genderGroup.clearCheck();
+            lvlSp.setSelection(0);
+        });
     }
 }
