cdd8f09697272452646f6b579dfc483af72f9dd7
Kchau <Kchau061906@gmail.com>
2025-09-15 23:14:16 -0500

    Spinner Values

--- app/src/main/java/com/example/pokemon_tracker_from/MainActivity.java
+++ app/src/main/java/com/example/pokemon_tracker_from/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.pokemon_tracker_from;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +12,36 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_table);
+        setContentView(R.layout.activity_main_linear);
 
         EditText NatNumET = findViewById(R.id.NationalNumET);
         EditText nameET = findViewById(R.id.nameET);
         EditText speciesET = findViewById(R.id.speciesET);
+        RadioGroup genderGroup = findViewById(R.id.genderGroup);
         EditText heightET = findViewById(R.id.heightET);
         EditText weightET = findViewById(R.id.weightET);
+        Spinner lvlSp= findViewById(R.id.spinner);
+
+        // list of numbers from 1 to 50
+        List<String> numbers = new ArrayList<>();
+        for (int i = 1; i <= 50; i++) {
+            numbers.add(String.valueOf(i));
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                numbers
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        lvlSp.setAdapter(adapter);
 
     }
 }
